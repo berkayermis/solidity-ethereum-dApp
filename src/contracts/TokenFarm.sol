67d345d9b616af75c2ce84e4e42da3df54d2e17d
berkayermis <mberkayermis@gmail.com>
2021-07-26 01:46:32 +0300

    Yield Farming Constructor

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -1,7 +1,18 @@
 pragma solidity ^0.5.0;
 
+import "./DappToken.sol";
+import "./DaiToken.sol";
+
 contract TokenFarm{
 
 	string public name = "Dapp Token Farm";
+	DappToken public dappToken;
+	DaiToken public daiToken;
+
+	constructor(DappToken _dappToken, DaiToken _daiToken) public {
+		DappToken = _dappToken;
+		DaiToken = _daiToken;
+
+	}
 	
 }
