843bbd971a7deb46169f717a66c7f8e70167e0dc
berkayermis <mberkayermis@gmail.com>
2021-08-03 05:15:14 +0300

    test

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,6 +3,7 @@ const DaiToken = artifacts.require('DaiToken')
 const TokenFarm = artifacts.require('TokenFarm')
 
 module.exports = async function(deployer, network, accounts) {
+  
   // Deploy Mock DAI Token
   await deployer.deploy(DaiToken)
   const daiToken = await DaiToken.deployed()
