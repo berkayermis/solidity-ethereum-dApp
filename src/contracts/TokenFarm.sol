1bc09cbdaf8aa8a049043f44a17f34247b146f28
Berkay Ermis <berkayermis@pop-os.localdomain>
2021-08-03 05:09:46 +0300

    Tokens deployment and Transfers

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -5,14 +5,12 @@ import "./DaiToken.sol";
 
 contract TokenFarm{
 
-	string public name = "Dapp Token Farm";
-	DappToken public dappToken;
-	DaiToken public daiToken;
+    string public name = "Dapp Token Farm";
+    DappToken public dappToken;
+    DaiToken public daiToken;
 
-	constructor(DappToken _dappToken, DaiToken _daiToken) public {
-		DappToken = _dappToken;
-		DaiToken = _daiToken;
-
-	}
-	
+    constructor(DappToken _dappToken, DaiToken _daiToken) public {
+        dappToken = _dappToken;
+        daiToken = _daiToken;
+    }
 }
